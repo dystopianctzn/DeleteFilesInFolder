c46ea4934ee57d041e27187e30db1f5d47e23b14
dystopianctzn <kinda.toxic11@gmail.com>
2023-06-16 19:03:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//путь
+DirectoryInfo folder = new DirectoryInfo(@"C:\1");
+
+try
+{
+    if (folder.Exists)
+    {
+        foreach (FileInfo file in folder.GetFiles())
+        {
+            if (file.LastAccessTime < DateTime.Now.AddMinutes(-30))
+                file.Delete();
+        }
+
+
+        foreach (DirectoryInfo dir in folder.GetDirectories())
+        {
+            if (dir.LastAccessTime < DateTime.Now.AddMinutes(-30))
+                dir.Delete(true);
+        }
+    }
+
+    else
+        Console.WriteLine("Такой папки не существует");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Произошла ошибка: " + ex.Message);
+}
